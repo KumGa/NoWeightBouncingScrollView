497b0d7c7371a6e16f47d4f72cdb664ae3851a7b
KumGa <qumaxi@gmail.com>
2016-02-10 18:06:07 +0900

    UICollectionView Sample

--- NoWeightBouncingScrollView/NoWeightBouncingScrollView/ViewController.m
+++ NoWeightBouncingScrollView/NoWeightBouncingScrollView/ViewController.m
@@ -9,8 +9,10 @@
 #import "ViewController.h"
 #import "KGNoWeightBouncingHelper.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UISwitch *enableSwitch;
 @end
 
 @implementation ViewController {
@@ -23,18 +25,29 @@
     _helper = [[KGNoWeightBouncingHelper alloc]init];
     _helper.noBounceOffset = 300;
     
-    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
-    [refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
-    refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"refresh"];
-    self.refreshControl = refreshControl;
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self
+                        action:@selector(refresh:)
+              forControlEvents:UIControlEventValueChanged];
+    
+    self.refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:@"refresh"];
+    [self.collectionView addSubview:self.refreshControl];
+    self.refreshControl.layer.zPosition = -1;
 }
 
 - (void)refresh:(id)sender{
     [self.refreshControl beginRefreshing];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.refreshControl endRefreshing];
-        [self.tableView reloadData];
     });
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 500;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    return [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
 }
 
 #pragma - mark UIScrollViewDelegate
@@ -44,7 +57,9 @@
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    [_helper notifyDidScroll:scrollView];
+    if (_enableSwitch.isOn) {
+        [_helper notifyDidScroll:scrollView];
+    }
 }
 
 @end
